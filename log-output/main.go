875d747c35b2b5ac950ac42db61c3486059036ca
Dawid Lachowicz <domdaw7@gmail.com>
2022-06-02 21:59:31 +0100

    Extract hashgen from log-output

--- log-output/main.go
+++ log-output/main.go
@@ -2,22 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"strings"
 	"time"
+
+	"github.com/dawidl022/mooc-fi-kubernetes/hashgen"
 )
 
-func genRandomString(length int) string {
-	rand.Seed(time.Now().Unix())
-	builder := strings.Builder{}
-	for i := 0; i < length; i++ {
-		builder.WriteRune('a' + rune(rand.Intn(26)))
-	}
-	return builder.String()
-}
-
 func main() {
-	randString := genRandomString(40)
+	randString := hashgen.GenRandomHash(40)
 	for {
 		fmt.Printf("%s: %s\n", time.Now().Format(time.RFC3339), randString)
 		time.Sleep(time.Second * 5)
