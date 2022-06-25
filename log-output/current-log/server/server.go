45a887c09dad0aea5fc18134f106971909a51432
Dawid Lachowicz <domdaw7@gmail.com>
2022-06-25 07:37:20 +0100

    Add ping count reading and writing between apps

--- log-output/current-log/server/server.go
+++ log-output/current-log/server/server.go
@@ -19,7 +19,14 @@ func StartServer() {
 			http.Error(w, fmt.Sprintf("failed to read log file: %v", err), http.StatusInternalServerError)
 			return
 		}
+		ping_count, err := os.ReadFile("stats/ping_count")
+		if err != nil {
+			http.Error(w, fmt.Sprintf("failed to read ping count file: %v", err), http.StatusInternalServerError)
+			return
+		}
+
 		w.Write(current_log)
+		fmt.Fprintf(w, "Ping / Pongs: %s", ping_count)
 	})
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
--- ping-pong/server/server.go
+++ ping-pong/server/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func StartServer() {
@@ -13,11 +14,20 @@ func StartServer() {
 		port = "8080"
 	}
 	counter := 0
+	writeRequestCount(counter)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "pong %d", counter)
 		counter++
+		writeRequestCount(counter)
 	})
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func writeRequestCount(count int) {
+	err := os.WriteFile("stats/ping_count", []byte(strconv.Itoa(count)), 0644)
+	if err != nil {
+		log.Println(err)
+	}
 }
