d6fe923f57e5f76cf4c8a7f7aa8658022b3ca1ae
Dawid Lachowicz <domdaw7@gmail.com>
2022-06-25 09:34:31 +0100

    Add endpoint to kill server for testing k8s restart

--- todo/server/routes.go
+++ todo/server/routes.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/dawidl022/mooc-fi-kubernetes/todo/server/handlers"
 )
@@ -9,4 +10,8 @@ import (
 func routes() {
 	http.Handle("/hash", handlers.NewHashHandler())
 	http.Handle("/daily-image", handlers.NewDailyImageHandler())
+
+	http.HandleFunc("/kill", func(w http.ResponseWriter, r *http.Request) {
+		os.Exit(0)
+	})
 }
