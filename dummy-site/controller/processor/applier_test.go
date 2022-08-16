0ee062f5ddea35cfd4d405a43f057809f0620b85
Dawid Lachowicz <domdaw7@gmail.com>
2022-08-16 18:50:43 +0100

    Add tests covering applier loop errors

--- dummy-site/controller/processor/applier_test.go
+++ dummy-site/controller/processor/applier_test.go
@@ -1,6 +1,7 @@
 package processor
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -13,6 +14,15 @@ const (
 	actionApply   applierAction = "APPLY"
 	actionCleanup applierAction = "CLEANUP"
 )
+
+type applierStub struct{}
+
+func (a *applierStub) apply(*manifests) error            { return nil }
+func (a *applierStub) cleanupResources(*manifests) error { return nil }
+
+func (a *applierStub) sleepDuration() time.Duration {
+	return 5 * time.Millisecond
+}
 
 type applierSpy struct {
 	actions []applierAction
@@ -27,15 +37,6 @@ func (a *applierSpy) apply(*manifests) error {
 func (a *applierSpy) cleanupResources(*manifests) error {
 	a.actions = append(a.actions, actionCleanup)
 	return nil
-}
-
-type applierStub struct{}
-
-func (a *applierStub) apply(*manifests) error            { return nil }
-func (a *applierStub) cleanupResources(*manifests) error { return nil }
-
-func (a *applierStub) sleepDuration() time.Duration {
-	return 5 * time.Millisecond
 }
 
 func TestApplyUntilDestroyed_RunsUtilTerminated(t *testing.T) {
@@ -61,6 +62,68 @@ func TestApplyUntilDestroyed_ReportsStatus(t *testing.T) {
 	time.Sleep(3*a.sleepDuration() - time.Millisecond)
 	term <- true
 
+	assertStatus(t, []status{
+		StatusWorking, StatusWorking, StatusWorking, StatusDone,
+	}, statusChan)
+}
+
+type applierErroringStub struct {
+	failNext bool
+	applierStub
+}
+
+func (a *applierErroringStub) apply(*manifests) error {
+	var res error
+	if a.failNext {
+		res = errors.New("stub ordered to fail")
+	}
+	a.failNext = !a.failNext
+	return res
+}
+
+func TestApplyUntilDestroyed_ReportsErrors(t *testing.T) {
+	a := applierErroringStub{}
+	term := make(chan bool)
+	statusChan := make(chan status, 5)
+	go applyUntilDestroyed(&a, &manifests{}, term, statusChan)
+
+	time.Sleep(4*a.sleepDuration() - time.Millisecond)
+	term <- true
+
+	assertStatus(t, []status{
+		StatusWorking, StatusError, StatusWorking, StatusError, StatusDone,
+	}, statusChan)
+}
+
+type applierErrorTermStub struct {
+	failCount int
+	applierStub
+}
+
+func (a *applierErrorTermStub) cleanupResources(*manifests) error {
+	var res error
+	if a.failCount < 3 {
+		res = errors.New("stub ordered to fail")
+	}
+	a.failCount++
+	return res
+}
+
+func TestApplyUntilDestroyed_RetriesTermination(t *testing.T) {
+	a := applierErrorTermStub{}
+	term := make(chan bool)
+	statusChan := make(chan status, 5)
+	go applyUntilDestroyed(&a, &manifests{}, term, statusChan)
+
+	time.Sleep(2*a.sleepDuration() - time.Millisecond)
+	term <- true
+
+	assertStatus(t, []status{
+		StatusWorking, StatusWorking, StatusError, StatusError, StatusError, StatusDone,
+	}, statusChan)
+}
+
+func assertStatus(t *testing.T, expected []status, statusChan chan status) {
 	var read []status
 	for {
 		status, ok := <-statusChan
@@ -71,7 +134,5 @@ func TestApplyUntilDestroyed_ReportsStatus(t *testing.T) {
 		}
 	}
 
-	assert.Equal(t, []status{
-		StatusWorking, StatusWorking, StatusWorking, StatusDone,
-	}, read)
+	assert.Equal(t, expected, read)
 }
